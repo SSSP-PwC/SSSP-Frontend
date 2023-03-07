98c71ecc9d152421d79f8ea01dccab03ae128275
Marcas O Donnell <modonnell29@qub.ac.uk>
2023-03-07 16:36:02 +0000

    Made changes to page builder

--- src/Components/FormStages/Scheme/PageBuilder.js
+++ src/Components/FormStages/Scheme/PageBuilder.js
@@ -1,31 +1,131 @@
 import { Divider, TextField } from "@mui/material";
-import { Button, Checkbox, Heading, InputField, Select } from "govuk-react";
+import { Button, Checkbox, Heading, InputField, Select, TextArea } from "govuk-react";
 import React, { useState } from "react";
 import { Tab, Tabs } from "react-bootstrap";
 import { MainHeading } from "../../../globalStyles";
 import FormBuilder from "./FormBuilder";
-
+import { useNavigate } from "react-router-dom";
 const PageBuilder = () => {
   const [pageElements, setPageElements] = useState([]);
   const [pageTitle, setPageTitle] = useState("");
   const [pageUrl, setPageUrl] = useState("");
-  const [navbarTitle, setNavbarTitle] = useState("");
+  const [fields, setFields] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+  const [activeTab, setActiveTab] = useState(0);
+  const [numTabs, setNumTabs] = useState(1);
+  const [tabs, setTabs] = useState([
+    {
+      id: 1,
+      title: "Portal",
+      content: "",
+      fields: [
+        {
+          label: "",
+          type: "string",
+          required: false,
+          button_link: "",
+        },
+      ],
+    },
+  ]);
+  const navigate = useNavigate();
+  const showPortal = () => {
+    setShowForm(true);
+  };
+  const handleNavigate = () => {
+    navigate(`${tabs[activeTab].fields[activeTab].button_link}`);
+  };
 
-  const [activeTab, setActiveTab] = useState(1);
-  const [numTabs, setNumTabs] = useState(0);
-  const [tabs, setTabs] = useState({
-   1: { id: 1, title: "Homepage", content: "Page 1", fields : [] },
-});
-  const [fields, setFields] = useState(tabs[activeTab].fields);
+  const renderForm = () => {
+    return (
+      <form>
+        {tabs[activeTab].fields.map((field, index) => (
+          <div key={index}>
+            {field.type === "button" && (
+              <div>
+                <Button
+                  onClick={handleNavigate}
+                  type={field.type}
+                  name={field.label}
+                  required={field.required}
+                >
+                  {field.label}
+                </Button>
+              </div>
+            )}
+            {field.type === "string" && (
+              <div>
+                <label style={{ textAlign: "center" }}>{field.label}</label>
+                <br></br>
+                <InputField
+                  input={{
+                    type: field.type,
+                    name: field.label,
+                    required: field.required,
+                  }}
+                />
+              </div>
+            )}
+            {field.type === "password" && (
+              <div>
+                <label style={{ textAlign: "center" }}>{field.label}</label>
+                <br></br>
+                <InputField
+                  input={{
+                    type: field.type,
+                    name: field.label,
+                    required: field.required,
+                  }}
+                />
+              </div>
+            )}
+            {field.type === "number" && (
+              <div>
+                <label style={{ textAlign: "center" }}>{field.label}</label>
+                <br></br>
+                <InputField
+                  input={{
+                    type: field.type,
+                    name: field.label,
+                    required: field.required,
+                  }}
+                />
+              </div>
+            )}
+            {field.type === "checkbox" && (
+              <div>
+                <label style={{ textAlign: "center" }}>{field.label}</label>
+                <br></br>
+                <Checkbox name={field.label} required={field.required} />
+              </div>
+            )}
+            {field.type === "textarea" && (
+              <div>
+                <label style={{ textAlign: "center" }}>{field.label}</label>
+                <br></br>
+                <TextArea
+                  input={{
+                    type: field.type,
+                    name: field.label,
+                    required: field.required,
+                  }}
+                >
+                </TextArea>
+              </div>
+            )}
+          </div>
+        ))}
+      </form>
+    );
+  };
 
-    tabs= Array.from(tabs[1])
   const handleAddTab = () => {
     const newId = numTabs + 1;
     const newTab = {
       id: newId,
-      title: `Form  - Stage ${newId}`,
-      content: `Form - Stage ${newId} content`,
-      fields: [],
+      title: pageTitle,
+      content: fields[newId],
+      fields: [{}],
     };
     setTabs([...tabs, newTab]);
     setNumTabs(newId);
@@ -41,45 +141,60 @@ const PageBuilder = () => {
       id: newId,
       title: `Summary - Stage ${newId}`,
       content: `Summary ${newId} content`,
-      fields: [],
     };
     setTabs([...tabs, newTab]);
     setNumTabs(newId);
   };
+
   const handleAddField = () => {
-    const newFields = [...fields];
-    newFields.push({
+    const currentTab = tabs[activeTab];
+    const currentTabFields = currentTab.fields ? [...currentTab.fields] : [];
+    console.log(currentTab);
+    console.log(currentTabFields);
+
+    currentTabFields.push({
       label: "",
       type: "",
       required: false,
       button_link: "",
     });
-    setFields(newFields);
-  };
 
-  const savePage = () => {
-    const data = {
-      title: pageTitle,
-      url: pageUrl,
-      tabs: tabs,
+    const newTabs = [...tabs];
+    newTabs[activeTab] = {
+      ...currentTab,
+      fields: currentTabFields,
     };
-    console.log(data);
+    console.log(newTabs);
+
+    setTabs(newTabs);
+  };
+  const savePage = () => {
+    console.log(tabs);
   };
   const handleRemoveField = (index) => {
+    const currentTab = tabs[activeTab];
+    if (!currentTab) {
+      console.error(`Tab ${activeTab} is not defined`);
+      return;
+    }
+    const currentTabFields = [...currentTab.fields];
+    currentTabFields.splice(index, 1);
     const newTabs = [...tabs];
-    const tabIndex = newTabs.findIndex((tab) => tab.id === activeTab);
-    const newFields = [...newTabs[tabIndex].fields];
-    newFields.splice(index, 1);
-    newTabs[tabIndex].fields = newFields;
+    newTabs[activeTab] = {
+      ...currentTab,
+      fields: currentTabFields,
+    };
     setTabs(newTabs);
   };
 
   const handleUpdateField = (index, field) => {
+    const currentTabFields = [...tabs[activeTab].fields];
+    currentTabFields[index] = field;
     const newTabs = [...tabs];
-    const tabIndex = newTabs.findIndex((tab) => tab.id === activeTab);
-    const newFields = [...newTabs[tabIndex].fields];
-    newFields[index] = field;
-    newTabs[tabIndex].fields = newFields;
+    newTabs[activeTab] = {
+      ...tabs[activeTab],
+      fields: currentTabFields,
+    };
     setTabs(newTabs);
   };
 
@@ -91,176 +206,174 @@ const PageBuilder = () => {
     <div className="container">
       <MainHeading style={{ color: "#0B0C0C", fontWeight: "bold" }}>
         Page Builder
-      </MainHeading>{" "}
+      </MainHeading>
       <p style={{ color: "#505a5f" }}>
         Use this service to build your own pages.
       </p>
       <Divider style={{ background: "black" }}></Divider>
       <br></br>
-      <InputField
-        input={{
-          label: "Page Title",
-          value: pageTitle,
-          onChange: (e) => setPageTitle(e.target.value),
-        }}
-      >
-        Page Title
-      </InputField>
-      <br></br>
-      <InputField
-        input={{
-          label: "Page URL",
-          value: pageUrl,
-          onChange: (e) => setPageUrl(e.target.value),
-        }}
-      >
-        Page Endpoint
-      </InputField>
-      <br></br>
-      <InputField
-        input={{
-          label: "Navbar title",
-          value: navbarTitle,
-          onChange: (e) => setNavbarTitle(e.target.value),
-        }}
-      >
-        Navbar title
-      </InputField>
-      <br></br>
-      <Button
-        variant="contained"
-        onClick={handleAddField}
-        style={{ margin: "5px" }}
-      >
-        Add Element
-      </Button>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={savePage}
-        style={{ margin: "5px" }}
-      >
-        Save Page
-      </Button>
-      <Button onClick={handleAddTab} style={{ margin: "5px" }}>
-        + Add Page
-      </Button>
-      <Button onClick={handleAddSummary} style={{ margin: "5px" }}>
-        Submit
-      </Button>
-      <Button onClick={handleRemoveTab} style={{ margin: "5px" }}>
-        Delete tab
-      </Button>
-      <br></br>
-      {pageElements.map((element) => {
-        if (element.type === "text") {
-          return <div key={element.id}>{element.text}</div>;
-        } else if (element.type === "image") {
-          return (
-            <img
-              key={element.id}
-              src={element.imageUrl}
-              alt={element.altText}
-            />
-          );
-        } else if (element.type === "link") {
-          return (
-            <a key={element.id} href={element.linkUrl}>
-              {element.linkText}
-            </a>
-          );
-        }
-      })}
-      <br></br>
-      <Tabs activeKey={activeTab} onSelect={(key) => setActiveTab(key)}>
-        {tabs.map((tab) => (
-          <Tab
-            key={tab.id}
-            eventKey={tab.id}
-            title={
-              <>
-                
-                <p>{tab.title}</p>
-                <Button  onClick={() => handleRemoveTab(tab.id)}> x</Button>
-              </>
-            }
-            disabled={tab.disabled}
+      {showForm === false && (
+        <div>
+          <InputField
+            input={{
+              label: "Page Title",
+              value: pageTitle,
+              onChange: (e) => setPageTitle(e.target.value),
+            }}
           >
-            <br></br>
-            {fields.map((field, index) => (
-              <div key={index}>
-                <InputField
-                  input={{
-                    type: "text",
-                    value: field.label,
-                    onChange: (e) =>
-                      handleUpdateField(index, {
-                        ...field,
-                        label: e.target.value,
-                      }),
-                  }}
-                >
-                  Label:
-                </InputField>
+            Page Title
+          </InputField>
+          <br></br>
+          <InputField
+            input={{
+              label: "Page URL",
+              value: pageUrl,
+              onChange: (e) => setPageUrl(e.target.value),
+            }}
+          >
+            Page Endpoint
+          </InputField>
+          <br></br>
 
+          <Button
+            variant="contained"
+            onClick={handleAddField}
+            style={{ margin: "5px" }}
+          >
+            Add Element
+          </Button>
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={savePage}
+            style={{ margin: "5px" }}
+          >
+            Save Page
+          </Button>
+          <Button onClick={handleAddTab} style={{ margin: "5px" }}>
+            + Add Page
+          </Button>
+          <Button onClick={showPortal} style={{ margin: "5px" }}>
+            Preview
+          </Button>
+          <br></br>
+          {pageElements.map((element) => {
+            if (element.type === "text") {
+              return <div key={`element-${element.id}`}>{element.text}</div>;
+            } else if (element.type === "image") {
+              return (
+                <img
+                  key={element.id}
+                  src={element.imageUrl}
+                  alt={element.altText}
+                />
+              );
+            } else if (element.type === "link") {
+              return (
+                <a key={`element${element.id}`} href={element.linkUrl}>
+                  {element.linkText}
+                </a>
+              );
+            }
+          })}
+          <br></br>
+          <Tabs activeKey={activeTab} onSelect={(key) => setActiveTab(key)}>
+            {tabs.map((tab, index) => (
+              <Tab
+                key={`tab-${index}`}
+                eventKey={index}
+                title={
+                  <>
+                    <p>{tab.title}</p>
+                  </>
+                }
+                disabled={tab.disabled}
+              >
                 <br></br>
-                <Select
-                  value={field.type}
-                  label="Type:"
-                  onChange={(e) =>
-                    handleUpdateField(index, { ...field, type: e.target.value })
-                  }
-                >
-                  <option value="string">Text</option>
-                  <option value="string">Email</option>
-                  <option value="number">Number</option>
-                  <option value="button">Button</option>
-                  <option value="password">Password</option>
-                  <option value="checkbox">Checkbox</option>
-                </Select>
+                {console.log(fields)}
+                {tabs[activeTab].fields.map((field, index) => (
+                  <div key={`field-${index}`}>
+                    <InputField
+                      input={{
+                        type: "text",
+                        value: field.label,
+                        onChange: (e) =>
+                          handleUpdateField(index, {
+                            ...field,
+                            label: e.target.value,
+                          }),
+                      }}
+                    >
+                      Label:
+                    </InputField>
+                    <br></br>
 
-                <br></br>
-                <label>
-                  Required:
-                  <Checkbox
-                    onChange={(e) =>
-                      handleUpdateField(index, {
-                        ...field,
-                        required: e.target.checked,
-                      })
-                    }
-                    input={{
-                      type: "checkbox",
-                      checked: field.required,
-                    }}
-                  />
-                </label>
-                <br></br>
-                {field.type === "button" && (
-                  <InputField
-                    input={{
-                      type: "text",
-                      value: field.button_link,
-                      onChange: (e) =>
+                    <Select
+                      value={field.type}
+                      label="Type:"
+                      onChange={(e) =>
                         handleUpdateField(index, {
                           ...field,
-                          button_link: e.target.value,
-                        }),
-                    }}
-                  >
-                    {" "}
-                    Button Link:
-                  </InputField>
-                )}
-                <br></br>
-                <br></br>
-                <Button onClick={() => handleRemoveField(index)}>Remove</Button>
-              </div>
+                          type: e.target.value,
+                        })
+                      }
+                    >
+                      <option value="string">Text Field</option>
+                      <option value="string">Email</option>
+                      <option value="textarea">Textbox</option>
+                      <option value="number">Number</option>
+                      <option value="button">Button</option>
+                      <option value="password">Password</option>
+                      <option value="checkbox">Checkbox</option>
+                    </Select>
+
+                    <br></br>
+                    <label>
+                      Required:
+                      <Checkbox
+                        onChange={(e) =>
+                          handleUpdateField(index, {
+                            ...field,
+                            required: e.target.checked,
+                          })
+                        }
+                        input={{
+                          type: "checkbox",
+                          checked: field.required,
+                        }}
+                      />
+                    </label>
+                    <br></br>
+                    {field.type === "button" && (
+                      <InputField
+                        input={{
+                          type: "text",
+                          value: field.button_link,
+                          onChange: (e) =>
+                            handleUpdateField(index, {
+                              ...field,
+                              button_link: e.target.value,
+                            }),
+                        }}
+                      >
+                        {" "}
+                        Button Link:
+                      </InputField>
+                    )}
+                    <br></br>
+                    <br></br>
+                    <Button onClick={() => handleRemoveField(index)}>
+                      Remove
+                    </Button>
+                  </div>
+                ))}
+              </Tab>
             ))}
-          </Tab>
-        ))}
-      </Tabs>
-      <br></br>
+          </Tabs>
+          <br></br>
+        </div>
+      )}
+      {showForm === true && <div>{renderForm()}</div>}
     </div>
   );
 };
