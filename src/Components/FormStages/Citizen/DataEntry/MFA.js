203841a351b5af2226e6d77bfa0169415b581f20
AdamLogan12 <adam.x.logan@pwc.com>
2023-06-21 12:41:40 +0100

    Revert "added first row to home page"
    This reverts commit fbb6dd6f30355afdd3380c4f35e9125aa2f2576f.

--- src/Components/FormStages/Citizen/DataEntry/MFA.js
+++ src/Components/FormStages/Citizen/DataEntry/MFA.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Form, Alert } from "react-bootstrap";
 import { MainHeading } from "../../../../globalStyles";
 import { useForm } from "react-hook-form";
@@ -32,17 +32,7 @@ export const MFA = () => {
 
   const [data, setData] = useState({});
 
-  const [otpQr, setotpQr] = useState("https://www.google.com/chart?chs=200x200&chld=M|0&cht=qr&chl=otpauth://totp/SSSP:adam.x.logan%40pwc.com?secret=3KMMYWWOYNNRRG6MIOZUQ2GVJCGZRCN4&issuer=SSSP");
-
-  const fetchQR = async () => {
-    fetch(`${process.env.REACT_APP_BACKEND_URL}/genqrcode/adam.x.logan@pwc.com`)
-        .then((res) => res.json())
-        .then((data) => setotpQr(data.message))
-  }
-
-  useEffect(() => {
-    fetchQR();
-  }, []);
+  const [otpQr, setotpQr] = useState("");
 
   const handleVerifyClick = () => {
     verify(verificationType);
@@ -384,10 +374,9 @@ export const MFA = () => {
       setRenderToken(true);
       setVerificationCodeField(true);
       setToken("micorsoft token");
-
-      // fetch(`${process.env.REACT_APP_BACKEND_URL}/genqrcode/adam.x.logan@pwc.com`)
-      //   .then((res) => res.json())
-      //   .then((data) => setotpQr(data.message))
+      fetch(`http://127.0.0.1:2000/api/testget/`)
+        .then((res) => res.json())
+        .then((data) => setotpQr(data.message))
     }
   }
 
@@ -585,8 +574,6 @@ export const MFA = () => {
                     <p style={{ color: "#505a5f" }}>
                       Please scan the below QR code within either the Google Authenticator app or the Microsoft Authenticator app.
                     </p>
-
-                    {console.log("hello " + otpQr)}
 
                     <img src={`${otpQr}`} alt="QR Code for authenticator application" />
                     
